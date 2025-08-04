d1df6ff9c1544424cb6313e2e9a24b8671b40adb
Shyam Thakur <er.shyam.work@gmail.com>
2025-08-04 23:34:33 +0530

    JAVA DAY 1

---
+++ 1.first-idea-program/Inputs.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Inputs{
+    public static void main(String[]args){
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your roll number:");
+        int rollNo = input.nextInt();
+        System.out.println("your roll number is:"+rollNo);
+        
+        // System.out.println("Enter your name:");
+        // String name = input.nextLine();
+        // System.out.println("your name is:"+name);
+        
+        // System.out.println("enter float value:");
+        // float value = input.nextFloat();
+        // System.out.println("float value is :"+value);
+    }
+}
---
+++ 1.first-idea-program/Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives
+{
+	public static void main(String[] args) {
+	//	int  roll_no = 64;
+	//	char letter = 'r';
+	//	float marks = 98.67f;
+	//	double large_decimal_number =4567654.4567;
+	//	long large_integer = 34567834567876543l;
+	//	boolean check=false;
+	
+	}
+}
