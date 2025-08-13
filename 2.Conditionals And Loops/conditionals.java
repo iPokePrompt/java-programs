032bf48b4cac6e80d8275ce8be6b79b283c5db1e
Shyam Thakur <er.shyam.work@gmail.com>
2025-08-13 23:55:52 +0530

    LEARNED LOOPS, CHECKCASES AND SOME IMP QUESTIONS

---
+++ 2.Conditionals And Loops/conditionals.java
@@ -0,0 +1,27 @@
+public class conditionals{
+    public static void main(String[]args){
+        int salary =100000;
+        if(salary>500000){
+            salary += 10000;
+        }
+        else{
+            salary += 5000;
+        }
+        System.out.println("the updated salary is :"+salary);
+    }
+}
+
+/*public class conditionals2{
+    public static void main(String[] args) {
+        int salary = 200000;
+        if(salary > 500000){
+            salary+= 10000;
+        }else if(salary > 100000){
+            salary += 5000;
+        }
+        else{
+            salary += 2000;
+        }
+        System.out.println("the updated salary is : " + salary);
+    }
+}*/
---
+++ 2.Conditionals And Loops/loops.java
@@ -0,0 +1,63 @@
+ 
+public class loops{
+    public static void main(String[] args) {
+       for(int i=1;i<=5;i++){
+        System.out.println("hello world"+i);
+       }
+    }
+}
+
+/*wap to find the greatest among 3 non-sealed 
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	int a = in.nextInt();
+	int b = in.nextInt();
+	int c = in.nextInt();
+	
+	int max=a;
+	if(b>a){
+	    max=b;
+	}
+	if(c>b){
+	    max=c;
+	}
+	System.out.println("max:"+max);
+	
+	}
+	
+}*/
+
+
+/* one more - {
+	public static void main(String[] args) {
+
+	System.out.println(Math.max(34,67)); 
+	}
+}*/
+
+
+
+/*import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	int a = in.nextInt();
+	int b = in.nextInt();
+    int c = in.nextInt();
+	int max = Math.max(c,Math.max(a,b));
+
+	System.out.println(max); 
+	}	
+}*/
+
+/*public class loops2{
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++){
+			System.out.println("hello world"+i);
+		}
+	}
+}*/
