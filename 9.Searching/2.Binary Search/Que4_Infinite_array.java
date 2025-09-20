c62966e70516a8fcdb32d7b6cc9b6f487eeb2b57
Shyam Thakur <er.shyam.work@gmail.com>
2025-09-20 23:39:38 +0530

    Practise Binary  Search Questions - peak index in mountain array, finding in mountain array, finding in rotated binary search

--- 9.Searching/2.Binary Search/Que4_Infinite_array.java
+++ 9.Searching/2.Binary Search/Que4_Infinite_array.java
@@ -1,5 +1,5 @@
 
-// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/      leetcode 702
 public class Que4_Infinite_array {
     public static void main(String[] args) {
         int[] arr = {3, 5, 7, 9, 10, 90,
