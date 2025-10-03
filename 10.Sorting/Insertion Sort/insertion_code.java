9b2be326d93d1c46b764994c769e35b100b459d2
Shyam Thakur <er.shyam.work@gmail.com>
2025-10-03 23:08:30 +0530

    edit:changed the logic in insertion sort

--- 10.Sorting/Insertion Sort/insertion_code.java
+++ 10.Sorting/Insertion Sort/insertion_code.java
@@ -8,20 +8,17 @@ public class insertion_code {
     }
 
     static void insertionSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 1; i < n; i++) {
-            int key = arr[i];  // element to be placed at correct position
-            int j = i - 1;
-
-            // Shift elements that are greater than key
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+        for (int i = 1; i < arr.length; i++) {
+            for(int j = i; j > 0; j--) {
+                if(arr[j] < arr[j - 1]) {
+                    // Swap arr[j] and arr[j-1]
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
             }
-
-            // Place key at its correct position
-            arr[j + 1] = key;
         }
     }
 }
